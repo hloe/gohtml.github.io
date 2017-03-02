48672d5d8a549b785504b550669af9ef06d52a37
Larysa Goroshanska <goroshanska@gmail.com>
2017-03-02 12:20:12 +0200

    Homework7-8 is corrected (add title instead of data-title in HTML

--- js/homework7-8/form/js/form.js
+++ js/homework7-8/form/js/form.js
@@ -2,23 +2,26 @@ $('.input').mouseover(function () {
   // If we have already hint after clicking button, delete it:
   $('.hint').remove();
 
-  var title = $(this).attr('data-title');
+  var title = $(this).attr('title');
+  $(this).removeAttr('title');
+  $(this).data('data-title', title);
   $(this).closest('li').append('<span class="hint">' + title + '</span>');
 });
 
-
 $('.input').mouseout(function () {
   $('.hint').remove();
+  var dataTitle = $(this).data('data-title');
+  $(this).removeAttr('data-title');
+  $(this).attr('title', dataTitle);
 });
 
-//
 
 $('.button').click(function () {
   // If we have already hint after clicking button, delete it:
   $('.hint').remove();
 
   $('.input').each(function () {
-    var title = $(this).attr('data-title');
+    var title = $(this).attr('title');
     $(this).closest('li').append('<span class="hint">' + title + '</span>');
   });
   return false;
