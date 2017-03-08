c1f77be33d862875722472679bbd5623cfc582d2
Larysa Goroshanska <goroshanska@gmail.com>
2017-03-08 20:40:23 +0200

    Correct select plugin's name and add stop() to menu

--- js/homework9-10/menu_by_jQuery/js/showMenu.js
+++ js/homework9-10/menu_by_jQuery/js/showMenu.js
@@ -2,48 +2,59 @@ $(function () {
 
   // Show hidden submenu
   $('.dropdown').hover(function () {
-    $(this).children('.submenu').slideDown(500);
+    $(this).children('.submenu').stop(false, true).slideDown(500);
   }, function () {
-    $(this).children('.submenu').slideUp(500);
+    $(this).children('.submenu').stop(false, true).slideUp(500);
   });
 
   //Change menu items view
   $('.menu-item').hover(function () {
-    $(this).animate({
+    $(this).stop(false, true).animate({
       backgroundColor: '#dac96a',
       color: '#554c00',
     }, 500);
 
-    $(this).children('.menu-link').animate({
+    $(this).children('.menu-link').stop(false, true).animate({
       color: '#554c00',
-    }, 500);
+    }, {
+      queue: false,
+      duration: 500
+    });
 
   }, function () {
-    $(this).animate({
+    $(this).stop(false, true).animate({
       backgroundColor: '#492e74',
       color: '#1a053a',
     }, 500);
-    $(this).children('.menu-link').animate({
+    $(this).children('.menu-link').stop(false, true).animate({
       color: '#1a053a',
-    }, 500);
-
+    }, {
+      queue: false,
+      duration: 500
+    });
   });
 
-  //  // Change submenu items view
+  // Change submenu items view
   $('.submenu-item').hover(function () {
-    $(this).animate({
+    $(this).stop(false, true).animate({
       backgroundColor: '#dac96a',
     }, 500);
-    $(this).children('.menu-link').animate({
+    $(this).stop(false, true).children('.menu-link').animate({
       color: '#554c00',
-    }, 500);
+    }, {
+      queue: false,
+      duration: 500
+    });
   }, function () {
-    $(this).animate({
+    $(this).stop(false, true).animate({
       backgroundColor: '#492e74',
     }, 500);
-    $(this).children('.menu-link').animate({
+    $(this).stop(false, true).children('.menu-link').animate({
       color: '#1a053a',
-    }, 500);
+    }, {
+      queue: false,
+      duration: 500
+    });
   });
 
 
