9d4118f769be872a06bf783d044a245c6bb3cd47
Larysa Goroshanska <goroshanska@gmail.com>
2017-03-10 12:44:43 +0200

    Correct LoDash template and carousel

--- js/homework11-12/templateLoDash/js/script.js
+++ js/homework11-12/templateLoDash/js/script.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var quotes = [
   {
     quote: 'I used to be with it. But then they changed what it was.Now what I\'m with isn\'t it, and what\'s it seems scary and wierd. It\'ll happen to you.',
