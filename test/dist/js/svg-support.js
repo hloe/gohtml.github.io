69a66fdcf28d6b1772943787ae9a6172e70426e5
Larysa <goroshanska@gmail.com>
2017-05-03 02:06:05 +0300

    Header is done

--- test/dist/js/svg-support.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-let arrowDown = document.getElementsByClassName('arrow-down')[0];
-arrowDown.onerror = function () {
-  arrowDown.setAttribute('src', 'images/yellow-arrow.png');
-  arrowDown.setAttribute('data-rjs', '2');
-};
