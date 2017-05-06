066b149d400f0b001deb35f9b46167d77ddb3833
Larysa <goroshanska@gmail.com>
2017-05-06 18:28:03 +0300

    Add styles for TESTIMONIALS

--- test/dist/js/tabs.js
+++ test/dist/js/tabs.js
@@ -1,8 +1,18 @@
 'use strict';
 
 $('.tabs__link').click(function () {
-  $('.tabs__link').removeClass('active');
-  $(this).addClass('active');
+  $('.tabs__link').removeClass('active-tab');
+  $(this).addClass('active-tab');
+  var id = $(this).attr('href');
+  $(id).removeClass('hidden');
+  $(id).siblings('.tabs__content').addClass('hidden');
+
+  return false;
+})
+
+$('.tabs__link').focus(function () {
+  $('.tabs__link').removeClass('active-tab');
+  $(this).addClass('active-tab');
   var id = $(this).attr('href');
   $(id).removeClass('hidden');
   $(id).siblings('.tabs__content').addClass('hidden');
