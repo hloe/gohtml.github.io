abc35d2c33ee893e07933e6f9e99fbc1fd1c1f46
Larysa <goroshanska@gmail.com>
2017-05-07 18:33:45 +0300

    Add versions with beans-parallax

---
+++ test/without beans-parallax/app/js/tabs.js
@@ -0,0 +1,24 @@
+(function () {
+  'use strict';
+
+  $('.tabs__link').click(function () {
+    $('.tabs__link').removeClass('active-tab');
+    $(this).addClass('active-tab');
+    var id = $(this).attr('href');
+    $(id).removeClass('hidden');
+    $(id).siblings('.tabs__content').addClass('hidden');
+
+    return false;
+  })
+
+  $('.tabs__link').focus(function () {
+    $('.tabs__link').removeClass('active-tab');
+    $(this).addClass('active-tab');
+    var id = $(this).attr('href');
+    $(id).removeClass('hidden');
+    $(id).siblings('.tabs__content').addClass('hidden');
+
+    return false;
+  })
+
+})(jQuery);
