9d48918a5d1f42351eae80ad4db700a7c7203dce
Larysa <goroshanska@gmail.com>
2017-08-03 17:52:20 +0300

    Add third product version

--- studying/react/finecoding-with-react/app/components/App.js
+++ studying/react/finecoding-with-react/app/components/App.js
@@ -3,8 +3,6 @@ const React = require('react');
 const ReactRouter = require('react-router-dom');
 const Router = ReactRouter.BrowserRouter;
 const Route = ReactRouter.Route;
-//const browserHistory = ReactRouter.browserHistory;
-//const IndexRoute = ReactRouter.IndexRoute;
 const Switch = ReactRouter.Switch;
 
 const Home = require('./Home');
@@ -26,15 +24,16 @@ class App extends React.Component {
                 addLogo: true,
                 childClassName: 'menu header__menu'
               }}
-            />
-          
+            />     
+            <Switch>
+              <Route exact path="" component={Home} />
               <Route exact path="/" component={Home} />
+              <Route exact path="/home" component={Home} />
               <Route exact path="/works" component={Works} />
               <Route exact path="/about" component={About} />
-              <Route exact path="/hire" component={Hire} />
-            
-          </div>
-            
+              <Route exact path="/hire" component={Hire} /> 
+            </Switch>
+          </div>            
           <Footer />
         </div>
       </Router>      
