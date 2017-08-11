f0a822d7ba873bf58fb7d2b2624840cbac436f4b
Larysa <goroshanska@gmail.com>
2017-08-11 17:30:42 +0300

    Correct menu and svg in finecoding

--- projects/react/finecoding-with-react/app/components/App.js
+++ projects/react/finecoding-with-react/app/components/App.js
@@ -1,9 +1,9 @@
-const React = require('react');
+ const React = require('react');
 
-const ReactRouter = require('react-router-dom');
-const Router = ReactRouter.BrowserRouter;
-const Route = ReactRouter.Route;
-const Switch = ReactRouter.Switch;
+ const ReactRouter = require('react-router-dom');
+ const Router = ReactRouter.BrowserRouter;
+ const Route = ReactRouter.Route;
+ const Switch = ReactRouter.Switch;
 
 const Home = require('./Home');
 const Works = require('./Works');
@@ -24,14 +24,14 @@ class App extends React.Component {
                 addLogo: true,
                 childClassName: 'menu header__menu'
               }}
-            />     
+            />           
             <Switch>              
               <Route exact path="/home" component={Home} />
-              <Route exact path="/works" component={Works} />
-              <Route exact path="/about" component={About} />
-              <Route exact path="/hire" component={Hire} /> 
+              <Route path="/works" component={Works} />
+              <Route path="/about" component={About} />
+              <Route path="/hire" component={Hire} /> 
               <Route component={Home} />
-            </Switch>
+            </Switch> 
           </div>            
           <Footer />
         </div>
