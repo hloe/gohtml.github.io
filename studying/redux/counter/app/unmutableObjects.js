57bd8525c3b709397c4b3252b19ecc5d9d2fde3b
Larysa <goroshanska@gmail.com>
2017-09-09 22:06:36 +0300

    Add counter and todo-list by redux

---
+++ studying/redux/counter/app/unmutableObjects.js
@@ -0,0 +1,39 @@
+function () {
+const toggleTodo = (todo) => {
+//  return {
+//    id: todo.id,
+//    text: todo.text,
+//    completed: !todo.completed
+//  };
+  ////
+//  return Object.assign({}, todo, {
+//    completed: !todo.completed
+//  });
+  return {
+    ...todo,
+    completed: !todo.completed
+  };
+};
+
+const testToggleTodo = () => {
+  const todoBefore = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: false
+  };
+  const todoAfter = {
+    id: 0,
+    text: 'Learn Redux',
+    completed: true
+  };
+  
+  deepFreeze(todoBefore);
+  
+  expect(
+    toggleTodo(todoBefore).toEqual(todoAfter);
+  )
+};
+
+testToggleTodo();
+console.log('All tests passed.');
+}
