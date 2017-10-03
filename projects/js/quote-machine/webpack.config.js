240dd9088f1c44cf43cde4f35bf89f4813f70555
Larysa <goroshanska@gmail.com>
2017-10-03 20:23:45 +0300

    bundle.js is correct (fonts and images rendering)

--- projects/js/quote-machine/webpack.config.js
+++ projects/js/quote-machine/webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              name: 'images/[name].[ext]',
+              name: './images/[name].[ext]',
             },
           },
           'img-loader',
@@ -37,7 +37,7 @@ module.exports = {
           {
             loader: 'file-loader',
             options: {
-              name: 'fonts/[name].[ext]',
+              name: './fonts/[name].[ext]',
             },
           },
         ],
